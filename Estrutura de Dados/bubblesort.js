f4f0291178bdc76e41b19f5dfbc903c43590bb9f
GhostBlkz <111324297+GhostBlkz@users.noreply.github.com>
2023-03-07 11:07:39 -0300

    Add files via upload

--- Estrutura de Dados/bubblesort.js
+++ Estrutura de Dados/bubblesort.js
@@ -30,18 +30,7 @@ function direta(vetor) {
     }
 }
 
-function InserDireta1(vetor) {
-    for (let i = 1; i < vetor.length; i++) {
-        let atual = vetor[i]
-        let j
-        for (j = i - 1; j >= 0 && vetor[j] > atual; j--) {
-            vetor[j + 1] = vetor[j]
-        }
-        vetor[j + 1] = atual
 
-    }
-
-}
 function InserDireta2(vetor){
     for(let i = 0; i < vetor.length; i++){
         for(let j = 0; j < i; j++){
@@ -77,7 +66,6 @@ function vetRandom(rand){
 //--------------------------------
 let vetor = []
 let aux = []
-vetRandom(aux)
 
 vetOrdenado(vetor)
 console.log(vetor)
@@ -92,14 +80,6 @@ console.log(vetor)
 console.time('direta...')
 direta(vetor)
 console.timeEnd('direta...')
-console.log(vetor)
-
-vetor = []
-vetOrdenado(vetor)
-console.log(vetor)
-console.time('InserDireta1...')
-InserDireta1(vetor)
-console.timeEnd('InserDireta1...')
 console.log(vetor)
 
 vetor = []
@@ -130,16 +110,33 @@ console.log(vetor)
 vetor = []
 vetInvertido(vetor)
 console.log(vetor)
-console.time('InserDireta1...')
-InserDireta1(vetor)
-console.timeEnd('InserDireta1...')
-console.log(vetor)
-
-vetor = []
-vetInvertido(vetor)
-console.log(vetor)
 console.time('InserDireta2...')
 InserDireta2(vetor)
 console.timeEnd('InserDireta2...')
 console.log(vetor)
 //-------------------------------------
+vetRandom(vetor)
+aux = vetor
+let aux1 = vetor
+let aux2 = vetor
+
+vetor = aux
+console.log(vetor)
+console.time('bubbleSort...')
+bubbleSort(vetor)
+console.timeEnd('bubbleSort...')
+console.log(vetor)
+
+vetor = aux1
+console.log(vetor)
+console.time('direta...')
+direta(vetor)
+console.timeEnd('direta...')
+console.log(vetor)
+
+vetor = aux2
+console.log(vetor)
+console.time('InserDireta2...')
+InserDireta2(vetor)
+console.timeEnd('InserDireta2...')
+console.log(vetor)
